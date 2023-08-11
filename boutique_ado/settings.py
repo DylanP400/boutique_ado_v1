0df5e5c2b2b8f02f4018f8deb97a121bafc08193
Dylan <Dylanmcmahon187@gmail.com>
2023-08-11 18:20:26 +0000

    completed homepage header & css

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-dylanp400-boutiqueadov1-w5oyw1migsa.ws-eu102.gitpod.io']
+ALLOWED_HOSTS = ['8000-dylanp400-boutiqueadov1-w5oyw1migsa.ws-eu103.gitpod.io']
 
 
 # Application definition
@@ -150,6 +150,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
